eded97f2375bab8778d02728bc8cafc583eb52fb
AnneBel44 <belouard@hotmail.com>
2024-08-22 12:14:35 +0200

    nettoyage repo

--- app_streamlit_joblib.py
+++ app_streamlit_joblib.py
@@ -15,10 +15,10 @@ import imblearn
 # Chargement du modèle
 model_pipeline = joblib.load("credit_scoring_new.joblib")
 
-# Extraire le modèle LGBM de la pipeline
+# Extraction du modèle LGBM de la pipeline
 model = model_pipeline.named_steps['model']
 
-# Charger les données de test reconstituées
+# Chargement des données de test reconstituées
 data_path = 'reconstituted_test_sampled.csv'
 test_data = pd.read_csv(data_path)
 
@@ -34,31 +34,31 @@ def display_shap_values(model, input_data):
     individual_shap = explainer.shap_values(input_data)
     shap_values_for_class_1 = individual_shap[1]
     # shap.initjs()
-    plt.figure(figsize=(25, 10))  # Augmenter la taille de la figure
+    plt.figure(figsize=(25, 10))  # Augmentation de la taille de la figure
     shap.waterfall_plot(shap.Explanation(values=shap_values_for_class_1[0], 
                                           base_values=explainer.expected_value[1], 
                                           data=input_data.iloc[0], 
                                           feature_names=input_data.columns.tolist()), show=False)
-    st.pyplot(plt, clear_figure=True)  # Ajuster la taille de l'affichage
+    st.pyplot(plt, clear_figure=True)  # Ajustement de la taille de l'affichage
 
 def st_shap(plot, height=400):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
-# Charger l'optimal_threshold depuis le fichier texte
+# Chargement de l'optimal_threshold depuis le fichier texte
 with open('optimal_threshold.txt', 'r') as f:
     optimal_threshold = float(f.read().strip())
 
 # Interface Streamlit
-st.sidebar.image("bannière.png", use_column_width=True)  # Ajouter la bannière en haut de la sidebar
+st.sidebar.image("bannière.png", use_column_width=True)  # Ajout de la bannière en haut de la sidebar
 
 st.markdown('<h1 style="text-align: center; color: #333333;">Credit Scoring</h1>', unsafe_allow_html=True)
 
-# Ajouter un espacement avant le contenu de la sidebar
-for _ in range(8):  # Ajouter plusieurs lignes vides pour augmenter l'espacement
+# Ajout d'un espacement avant le contenu de la sidebar
+for _ in range(8):  # Ajout de plusieurs lignes vides pour augmenter l'espacement
     st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
-# Changer la couleur de fond de la sidebar et la police en blanc
+# Changement de la couleur de fond de la sidebar et la police en blanc
 st.markdown(
     """
     <style>
@@ -111,9 +111,10 @@ st.markdown(
 
 # Contenu de la sidebar
 with st.sidebar:
-    # Ajouter le titre en majuscules
+    # Ajout du titre en majuscules
     st.markdown("<div class='sidebar-title'>CHOOSE CUSTOMER ID</div>", unsafe_allow_html=True)
     
+
     # Sélection de la méthode d'entrée
     input_method = st.radio("Choose input method:", ('Selectbox', 'Text Input'))
 
@@ -125,28 +126,30 @@ with st.sidebar:
         st.markdown("### **<span style='font-size:20px'>Enter Customer Id</span>**", unsafe_allow_html=True)
         selected_id = st.text_input("", value="")
 
-# Vérifiez si l'ID entré est valide
+# Vérification si l'ID entrée est valide
 if input_method == 'Selectbox' or (input_method == 'Text Input' and selected_id.isdigit()):
     selected_id = int(selected_id)
     if selected_id in test_data['SK_ID_CURR'].values:
         selected_data = test_data[test_data['SK_ID_CURR'] == selected_id].iloc[:, 1:]
 
-        # Générer les prédictions pour l'individu sélectionné
+        # Génération des prédictions pour l'individu sélectionné
         prediction_proba, prediction = generate_predictions(selected_data, optimal_threshold)
 
-        # Afficher le texte coloré en fonction de la prédiction
+        # Affichage du texte coloré en fonction de la prédiction
         if prediction[0] == 0:
             st.markdown("<div class='accepted-text'>ACCEPTED</div>", unsafe_allow_html=True)
         else:
             st.markdown("<div class='rejected-text'>REJECTED</div>", unsafe_allow_html=True)
 
-        # Séparer par une ligne
+        # Séparation par une ligne
         st.markdown("---")
 
-        # Ajouter un titre centré pour "Prediction Probability :"
-        st.markdown("<div class='centered-text'>Prediction Probability :</div>", unsafe_allow_html=True)
+        # Ajout d'un titre centré pour "Prediction Probability :"
+        st.markdown("<div class='centered-text'>Risk Probability :</div>", unsafe_allow_html=True)
+        st.markdown("<div class='risk-text'>If the customer obtains a score >= 0.09, we consider him to be risky (red line)</div>", unsafe_allow_html=True)
+
         
-        # Afficher les résultats sous forme de jauge
+        # Affichage des résultats sous forme de jauge
         fig = go.Figure(go.Indicator(
             mode="gauge+number",
             value=prediction_proba[0],
@@ -166,13 +169,11 @@ if input_method == 'Selectbox' or (input_method == 'Text Input' and selected_id.
         fig.update_layout(height=400)  # Réduire la hauteur de la jauge
         st.plotly_chart(fig, use_container_width=True)
 
-        # Ajouter le texte centré en gris sous la jauge
-        st.markdown("<div class='risk-text'>If the customer obtains a score greater than, or equal, to 0.09, we consider him to be risky</div>", unsafe_allow_html=True)
 
-        # Séparer par une ligne
+        # Séparation par une ligne
         st.markdown("---")
 
-        # Afficher les explications SHAP pour l'individu sélectionné
+        # Affichage des explications SHAP pour l'individu sélectionné
         st.markdown("<div class='centered-text'>Feature Importance for this prediction :</div>", unsafe_allow_html=True)
         display_shap_values(model, selected_data)
     else:
